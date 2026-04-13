a63d347d49001ab8fae1c69600d6709e78734f5f
Viorinite <viorinite@gmail.com>
2026-04-13 21:12:43 +0700

    Initial commit: NixOS configuration with sops-nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "NixOS configuration with flakes, Home Manager, and sops-nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    antigravity = {
+      url = "github:jacopone/antigravity-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, antigravity, sops-nix, ... }@inputs: {
+    nixosConfigurations = {
+      "Nixie" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          sops-nix.nixosModules.sops
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.nicole = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
